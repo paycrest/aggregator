47450b84c6ec3238b92491ddd310b44b33de769d
Chibuotu Amadi <chibuotu@gmail.com>
2025-02-05 03:05:46 +0100

    fix(tasks): proper types for sync fulfillments

--- tasks/tasks.go
+++ tasks/tasks.go
@@ -688,8 +688,8 @@ func SyncLockOrderFulfillments() {
 			// }
 
 			payload := map[string]interface{}{
-				"orderId":  order.ID,
-				"currency": order.Edges.ProvisionBucket.Edges.Currency,
+				"orderId":  order.ID.String(),
+				"currency": order.Edges.ProvisionBucket.Edges.Currency.String(),
 			}
 			// signature := tokenUtils.GenerateHMACSignature(payload, string(decryptedSecret))
 
@@ -788,8 +788,8 @@ func SyncLockOrderFulfillments() {
 					// }
 
 					payload := map[string]interface{}{
-						"orderId":  order.ID,
-						"currency": order.Edges.ProvisionBucket.Edges.Currency,
+						"orderId":  order.ID.String(),
+						"currency": order.Edges.ProvisionBucket.Edges.Currency.String(),
 						"psp":      fulfillment.Psp,
 						"txId":     fulfillment.TxID,
 					}
