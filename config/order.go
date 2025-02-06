10b9a853ed88171847539aee9d43b6292ca8b330
sundayonah <sundayonah94@gmail.com>
2025-02-06 12:09:01 +0100

    feat: added new `bundler_url` and `paymaster_url` columns to `networks` table

--- config/order.go
+++ config/order.go
@@ -21,17 +21,6 @@ type OrderConfiguration struct {
 	RefundCancellationCount          int
 	PercentDeviationFromExternalRate decimal.Decimal
 	PercentDeviationFromMarketRate   decimal.Decimal
-	BundlerUrlEthereum               string
-	PaymasterUrlEthereum             string
-	BundlerUrlPolygon                string
-	PaymasterUrlPolygon              string
-	BundlerUrlBase                   string
-	PaymasterUrlBase                 string
-	BundlerUrlBSC                    string
-	PaymasterUrlBSC                  string
-	BundlerUrlArbitrum               string
-	PaymasterUrlArbitrum             string
-	ActiveAAService                  string
 }
 
 // OrderConfig sets the order configuration
@@ -45,25 +34,12 @@ func OrderConfig() *OrderConfiguration {
 	viper.SetDefault("NETWORK_FEE", 0.05)
 	viper.SetDefault("PERCENT_DEVIATION_FROM_EXTERNAL_RATE", 0.01)
 	viper.SetDefault("PERCENT_DEVIATION_FROM_MARKET_RATE", 0.1)
-	viper.SetDefault("ACTIVE_AA_SERVICE", "stackup")
 
 	return &OrderConfiguration{
 		OrderFulfillmentValidity:         time.Duration(viper.GetInt("ORDER_FULFILLMENT_VALIDITY")) * time.Minute,
-		OrderRefundTimeout:               time.Duration(viper.GetInt("ORDER_REFUND_TIMEOUT")) * time.Minute,
 		ReceiveAddressValidity:           time.Duration(viper.GetInt("RECEIVE_ADDRESS_VALIDITY")) * time.Minute,
 		OrderRequestValidity:             time.Duration(viper.GetInt("ORDER_REQUEST_VALIDITY")) * time.Second,
 		TronProApiKey:                    viper.GetString("TRON_PRO_API_KEY"),
-		ActiveAAService:                  viper.GetString("ACTIVE_AA_SERVICE"),
-		BundlerUrlEthereum:               viper.GetString("BUNDLER_URL_ETHEREUM"),
-		PaymasterUrlEthereum:             viper.GetString("PAYMASTER_URL_ETHEREUM"),
-		BundlerUrlPolygon:                viper.GetString("BUNDLER_URL_POLYGON"),
-		PaymasterUrlPolygon:              viper.GetString("PAYMASTER_URL_POLYGON"),
-		BundlerUrlBase:                   viper.GetString("BUNDLER_URL_BASE"),
-		PaymasterUrlBase:                 viper.GetString("PAYMASTER_URL_BASE"),
-		BundlerUrlBSC:                    viper.GetString("BUNDLER_URL_BSC"),
-		PaymasterUrlBSC:                  viper.GetString("PAYMASTER_URL_BSC"),
-		BundlerUrlArbitrum:               viper.GetString("BUNDLER_URL_ARBITRUM"),
-		PaymasterUrlArbitrum:             viper.GetString("PAYMASTER_URL_ARBITRUM"),
 		EntryPointContractAddress:        common.HexToAddress(viper.GetString("ENTRY_POINT_CONTRACT_ADDRESS")),
 		BucketQueueRebuildInterval:       viper.GetInt("BUCKET_QUEUE_REBUILD_INTERVAL"),
 		RefundCancellationCount:          viper.GetInt("REFUND_CANCELLATION_COUNT"),
