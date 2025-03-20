fe0cdce9de83f76fc2e2d36b6f83060e0a23bdb8
sundayonah <sundayonah94@gmail.com>
2025-03-20 12:45:38 +0100

    refactor(config): remove Bitget API keys from AuthConfiguration

--- config/auth.go
+++ config/auth.go
@@ -14,9 +14,6 @@ type AuthConfiguration struct {
 	JwtRefreshLifespan    time.Duration
 	HmacTimestampAge      time.Duration
 	PasswordResetLifespan time.Duration
-	BitgetAccessKey       string
-	BitgetSecretKey       string
-	BitgetPassphrase      string
 }
 
 // AuthConfig sets the authentication & authorization configurations
@@ -25,9 +22,6 @@ func AuthConfig() (config *AuthConfiguration) {
 	viper.SetDefault("JWT_REFRESH_LIFESPAN", 10080) // 7 days
 	viper.SetDefault("HMAC_TIMESTAMP_AGE", 5)
 	viper.SetDefault("PASSWORD_RESET_LIFESPAN", 5)
-	viper.SetDefault("BITGET_ACCESS_KEY", "")
-	viper.SetDefault("BITGET_SECRET_KEY", "")
-	viper.SetDefault("BITGET_PASSPHRASE", "")
 
 	return &AuthConfiguration{
 		Secret:                viper.GetString("SECRET"),
@@ -35,9 +29,6 @@ func AuthConfig() (config *AuthConfiguration) {
 		JwtRefreshLifespan:    time.Duration(viper.GetInt("JWT_REFRESH_LIFESPAN")) * time.Minute,
 		HmacTimestampAge:      time.Duration(viper.GetInt("HMAC_TIMESTAMP_AGE")) * time.Minute,
 		PasswordResetLifespan: time.Duration(viper.GetInt("PASSWORD_RESET_LIFESPAN")) * time.Minute,
-		BitgetAccessKey:       viper.GetString("BITGET_ACCESS_KEY"),
-		BitgetSecretKey:       viper.GetString("BITGET_SECRET_KEY"),
-		BitgetPassphrase:      viper.GetString("BITGET_PASSPHRASE"),
 	}
 }
 
