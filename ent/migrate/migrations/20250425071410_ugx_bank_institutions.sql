14dde13e18fdf36c40c659a794d3478c50aa4311
sundayonah <sundayonah94@gmail.com>
2025-05-02 06:53:59 +0100

    fix: remove provision buckets from UGX and TZS bank institution migrations

--- ent/migrate/migrations/20250425071410_ugx_bank_institutions.sql
+++ ent/migrate/migrations/20250425071410_ugx_bank_institutions.sql
@@ -8,7 +8,6 @@ SELECT EXISTS (
 DO $$
 DECLARE
     fiat_currency_id UUID;
-    last_bucket_id BIGINT;
 BEGIN
     -- Get the ID of the UGX fiat currency
     SELECT "id" INTO fiat_currency_id
@@ -75,15 +74,4 @@ BEGIN
     SELECT "code", "name", fiat_currency_id, "type", "updated_at", "created_at"
     FROM institutions
     ON CONFLICT ("code") DO NOTHING;
-
-    -- Get the last bucket ID
-    SELECT COALESCE(MAX(id), 0) INTO last_bucket_id FROM provision_buckets;
-
-    -- Add provision buckets to the UGX fiat currency
-    INSERT INTO provision_buckets (id, min_amount, max_amount, created_at, fiat_currency_provision_buckets)
-    VALUES 
-        (last_bucket_id + 1, 0, 1000, now(), fiat_currency_id),
-        (last_bucket_id + 2, 1001, 5000, now(), fiat_currency_id),
-        (last_bucket_id + 3, 5001, 50000, now(), fiat_currency_id)
-    ON CONFLICT (id) DO NOTHING;
 END$$;
