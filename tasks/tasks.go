7036ab53f775ac52f641eddde5201e14921578e9
Chibuotu Amadi <chibuotu@gmail.com>
2025-06-28 08:54:34 +0100

    refactor: remove unnecessary logging in FixDatabaseMishap function

--- tasks/tasks.go
+++ tasks/tasks.go
@@ -887,8 +887,6 @@ func FixDatabaseMishap() error {
 	_ = indexerInstance.IndexOrderCreated(ctx, nil, network, 18052684, 18052684)
 	_ = indexerInstance.IndexOrderCreated(ctx, nil, network, 18056857, 18056857)
 
-	logger.Infof("FixDatabaseMishap: %v", ctx)
-
 	return nil
 }
 
