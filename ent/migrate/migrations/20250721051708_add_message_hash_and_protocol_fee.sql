6e8744e41f336825caab501dfe06b40bd228d13e
Chibuotu Amadi <chibuotu@gmail.com>
2025-07-21 06:51:50 +0100

    fix: set default value for protocol_fee in lock_payment_orders migration
    - Update the migration to add a default value of 0 for the protocol_fee column in the lock_payment_orders table.
    - Ensure the message_hash column remains nullable as previously defined.

--- ent/migrate/migrations/20250721051708_add_message_hash_and_protocol_fee.sql
+++ ent/migrate/migrations/20250721051708_add_message_hash_and_protocol_fee.sql
@@ -1,4 +1,4 @@
 -- Modify "lock_payment_orders" table
-ALTER TABLE "lock_payment_orders" ADD COLUMN "protocol_fee" double precision NOT NULL, ADD COLUMN "message_hash" character varying NULL;
+ALTER TABLE "lock_payment_orders" ADD COLUMN "protocol_fee" double precision NOT NULL DEFAULT 0, ADD COLUMN "message_hash" character varying NULL;
 -- Modify "payment_orders" table
 ALTER TABLE "payment_orders" DROP COLUMN "protocol_fee";
