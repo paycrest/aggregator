00f74d658b04e2274cf46239fcba1ab4d4ccca36
Chibuotu Amadi <chibuotu@gmail.com>
2025-11-27 17:23:20 +0100

    chore: update migration hash and add new OTC min/max migration script

---
+++ ent/migrate/migrations/20251127162259_add_min_max_otc.sql
@@ -0,0 +1,6 @@
+-- Modify "lock_payment_orders" table
+ALTER TABLE "lock_payment_orders" ADD COLUMN "order_type" character varying NOT NULL DEFAULT 'regular';
+-- Modify "payment_orders" table
+ALTER TABLE "payment_orders" ADD COLUMN "order_type" character varying NOT NULL DEFAULT 'regular';
+-- Modify "provider_order_tokens" table
+ALTER TABLE "provider_order_tokens" ADD COLUMN "max_order_amount_otc" double precision NOT NULL, ADD COLUMN "min_order_amount_otc" double precision NOT NULL;
