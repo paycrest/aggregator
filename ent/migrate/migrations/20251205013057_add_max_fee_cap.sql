92ae9a1d4ed51e2944e8708be016e8f19c326aee
Chibuotu Amadi <chibuotu@gmail.com>
2025-12-05 02:40:57 +0100

    chore(migrations): update migration hash and add new max fee cap migration

---
+++ ent/migrate/migrations/20251205013057_add_max_fee_cap.sql
@@ -0,0 +1,4 @@
+-- Modify "provider_order_tokens" table
+ALTER TABLE "provider_order_tokens" ALTER COLUMN "max_order_amount_otc" DROP DEFAULT, ALTER COLUMN "min_order_amount_otc" DROP DEFAULT;
+-- Modify "sender_order_tokens" table
+ALTER TABLE "sender_order_tokens" ADD COLUMN "max_fee_cap" double precision NOT NULL;
