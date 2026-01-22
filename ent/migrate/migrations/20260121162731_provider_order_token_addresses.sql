3cbe4f21c3ee27b935c50b2df86d2cd533869461
Chibuotu Amadi <chibuotu@gmail.com>
2026-01-22 15:36:43 +0100

    chore: remove deprecated provider_balances and provider_order_tokens migrations
    - Deleted the migration files for provider_balances and provider_order_tokens as they are no longer needed.
    - Updated atlas.sum to reflect the removal of these migrations.

--- ent/migrate/migrations/20260121162731_provider_order_token_addresses.sql
+++
@@ -1,5 +0,0 @@
--- Rename address column to settlement_address
-ALTER TABLE "provider_order_tokens" RENAME COLUMN "address" TO "settlement_address";
-
--- Add payout_address column
-ALTER TABLE "provider_order_tokens" ADD COLUMN "payout_address" character varying NULL;
