05c3a17e99f6df23d4401f6cb34ba4bdb0147b0a
Chibuotu Amadi <chibuotu@gmail.com>
2026-03-05 22:56:13 +0100

    fix: set default value for peak_balance in provider_balances migration
    - Updated the SQL migration for the provider_balances table to add a default value of 0.0 for the new peak_balance column, ensuring that existing records are initialized correctly and preventing potential null values.

--- ent/migrate/migrations/20260305193715_add_peak_balance.sql
+++ ent/migrate/migrations/20260305193715_add_peak_balance.sql
@@ -1,2 +1,2 @@
 -- Modify "provider_balances" table
-ALTER TABLE "provider_balances" ADD COLUMN "peak_balance" double precision NOT NULL;
+ALTER TABLE "provider_balances" ADD COLUMN "peak_balance" double precision NOT NULL DEFAULT 0.0;
