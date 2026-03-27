73aee13351defa1b2557ac80da3f1b1a01756131
Chibuotu Amadi <chibuotu@gmail.com>
2026-03-27 01:15:15 +0100

    feat(redis): enhance Redis configuration with TLS support and username option
    - Added REDIS_USERNAME and REDIS_TLS options to the .env.example file for improved Redis configuration.
    - Updated RedisConfiguration struct to include Username and UseTLS fields.
    - Modified RedisConfig function to set defaults for new configuration options.
    - Enhanced InitializeRedis function to support TLS connections based on the new configuration.

--- config/redis.go
+++ config/redis.go
@@ -10,17 +10,23 @@ import (
 type RedisConfiguration struct {
 	Host     string
 	Port     string
+	Username string
 	Password string
 	DB       int
+	UseTLS   bool
 }
 
 // RedisConfig retrieves the Redis configuration
 func RedisConfig() RedisConfiguration {
+	viper.SetDefault("REDIS_TLS", false)
+	viper.SetDefault("REDIS_USERNAME", "")
 	return RedisConfiguration{
 		Host:     viper.GetString("REDIS_HOST"),
 		Port:     viper.GetString("REDIS_PORT"),
+		Username: viper.GetString("REDIS_USERNAME"),
 		Password: viper.GetString("REDIS_PASSWORD"),
 		DB:       viper.GetInt("REDIS_DB"),
+		UseTLS:   viper.GetBool("REDIS_TLS"),
 	}
 }
 
